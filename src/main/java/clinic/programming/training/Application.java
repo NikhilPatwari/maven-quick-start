32fef549730015aaa2f066375803bdec87804ce5
NikhilPatwari <nikhilpatwari@hotmail.com>
2021-06-18 15:39:11 +0530

    pom file modified

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,26 @@
 package clinic.programming.training;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
-
+    public void greet()
+    {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("hello");
+        for(String S:greetings)
+        {
+            System.out.println("greetings :"+S);
+        }
+    }
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+        app.greet();
     }
 }
